58535e6c4d3690e58238bf89602b5c9a5df7f76d
Max <a4ron5@yandex.ru>
2018-02-15 18:51:51 +0500

    add defaultState

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,12 +1,26 @@
+import Initializator from '../Initialization/index'
 
+let defaultState = {
+  deck: Initializator.getDeck(),
+  count: 0,
+  isOpen: false,
+  selectedCards: [],
+  openedCardCards: [],
+  cardsInGame: [],
+  flag: true
+}
 
-function round(state, action) {
-    switch(action.type) {
-      case ROUND_WIN:
-        return Object.assign({}, state, {
-          count: 
-        })
-      case ROUND_LOSE:
-        return 
-    }
+export default function game(state = defaultState, action){
+  if(action.type === RESTART_GAME){
+    state = {
+      deck: Initializator.getDeck(),
+      count: 0,
+      isOpen: false,
+      selectedCards: [],
+      openedCardCards: [],
+      cardsInGame: [],
+      flag: true
+    };
+  }
+  
 }
