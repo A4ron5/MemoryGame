aa5e416caf0e9fd38b5c5de9095585ea6183bf61
Max <a4ron5@yandex.ru>
2018-02-28 21:21:06 +0500

    add props to cardlist feature

--- src/features/card-list/cards.js
+++ src/features/card-list/cards.js
@@ -11,10 +11,29 @@ class CardList extends React.Component {
     this.node.addEventListener("click", this.props.onClick);
   }
 
+  //TODO: метод исфлиппед
+
   render(){
+    const items = deck.map((item, index) => {
+      const name = `${item.rank}${item.suit}`;
+      return (
+        //TODO: изменить значения 
+        <Card
+          key={item.id}
+          id={index}
+          name={name}
+          image={item.image}
+          position={item.id}
+          //TODO: исопен на флиппед
+          isOpen={this.state.isOpen}
+          flag={this.props.flag}
+        /> 
+      )
+    })
+    
     return (
       <CardListView innerRef={this.handleDeck}>
-        <Card/>
+        {items}
       </CardListView>
     )
   }
