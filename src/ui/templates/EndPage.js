4b6761c5b53caefb7401a089290f55c79c8771a1
Max <a4ron5@yandex.ru>
2018-03-01 17:59:50 +0500

    add templates

---
+++ src/ui/templates/EndPage.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const EndPage = styled.div`
+  margin-top: 200px;
+`
---
+++ src/ui/templates/StartPage.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const StartPage = styled.div`
+  margin-top: 200px;
+`
